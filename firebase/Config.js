a4392776d041e6ed4e278295afe3f0445a83b44a
Enni Dahlström <n0daen00@students.oamk.fi>
2022-05-09 17:13:44 +0300

    Config tyhjennetty

--- firebase/Config.js
+++ firebase/Config.js
@@ -1,13 +1,6 @@
 import firebase from 'firebase/compat';
 
 const firebaseConfig = ({
-    apiKey: "AIzaSyBb7fEp0Shd4cdao1zADzfFEcRlXdpGbtw",
-    authDomain: "neuvolakortti-1dd02.firebaseapp.com",
-    databaseURL: "https://neuvolakortti-1dd02-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "neuvolakortti-1dd02",
-    storageBucket: "neuvolakortti-1dd02.appspot.com",
-    messagingSenderId: "451298330271",
-    appId: "1:451298330271:web:0be00e2f191e3b72f7c4aa"
   });
 
 const app = firebase.initializeApp(firebaseConfig);
